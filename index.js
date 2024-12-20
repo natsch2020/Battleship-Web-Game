a8efc9e24f5a2502f5ec384b0ca69206ec9db7bc
github.com-natsch2020 <natsch2020@gmail.com>
2024-12-20 13:37:35 -0600

    Webpack and babel-loader, "hello world" of sorts
    Installed webpack and babel-loader
    Created index.html and index.js
    Created webpack.config.js and .babelrc to allow webpack and babel-loader to function properly
    Browsers cannot natively understand import statements, thus the need for webpack and therefore babel-loader
    Needed to import Phaser into index.js
    Added a build script to package.json

---
+++ index.js
@@ -0,0 +1,26 @@
+import Phaser from 'phaser';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+      preload: preload,
+      create: create,
+      update: update
+    }
+};
+const game = new Phaser.Game(config);
+  
+function preload() {
+// Load assets here
+}
+
+function create() {
+// Initialize game objects here
+}
+
+function update() {
+// Handle game logic here
+}
+  
